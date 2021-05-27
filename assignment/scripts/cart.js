2b87564675ebe474836c3d4bfe0e44c712ce3bc7
Abram Chavez <abram.chavez820@gmail.com>
2021-05-27 14:23:40 -0500

    commit 3 functions w/ comment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,8 +5,20 @@ let basket = [];
 function addItem( item ){
   return true
 }
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
 
-function listItems
-for (var i = 0; i < basket.length; i++) {
+function listItems (){
+for (let i = 0; i < basket.length; i++) {
   basket[i]
 }
+return true;{
+}
+console.log('Test- items in basket',basket());
+}
+function empty (){
+  return empty
+}
+console.log(`Basket is now ${basket}`);
+// the console log 'Adding apples (expect true) true' is the only function that
+//returned an output...
